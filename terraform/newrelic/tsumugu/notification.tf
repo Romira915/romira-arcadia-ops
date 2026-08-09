bcb500f2bae924c195403af5d63f852b95a11672
Romira915 <40430090+Romira915@users.noreply.github.com>
2026-08-09 22:30:43 +0900

    tsumuguのSlack通知を既存workflowから分離する

--- terraform/newrelic/tsumugu/notification.tf
+++ terraform/newrelic/tsumugu/notification.tf
@@ -1,33 +1,42 @@
-locals {
-  # provider に notification channel の検索用 data source がないため、既存 workflow の channel ID を参照する。
-  hasunosora_twitter_watch_slack_channel_id = "58ee5e02-31c2-4e09-849c-616f5156a556"
+data "newrelic_notification_destination" "slack" {
+  id = "19d9283e-ce8d-459c-901e-eb179d421475"
 }
 
-import {
-  to = newrelic_workflow.hasunosora_twitter_watch_slack
-  id = "54524a31-f011-4253-b976-ff9ddc38abf5"
+resource "newrelic_notification_channel" "tsumugu_slack" {
+  name           = "tsumugu"
+  type           = "SLACK"
+  destination_id = data.newrelic_notification_destination.slack.id
+  product        = "IINT"
+
+  property {
+    key           = "channelId"
+    value         = "C06HR2WC9AA"
+    display_value = "newrelic"
+  }
+
+  property {
+    key   = "customDetailsSlack"
+    value = "@channel"
+  }
 }
 
-resource "newrelic_workflow" "hasunosora_twitter_watch_slack" {
-  name                  = "Policy 5087508: hasunosora-twitter-watch policy"
+resource "newrelic_workflow" "tsumugu" {
+  name                  = "Policy: tsumugu"
   muting_rules_handling = "DONT_NOTIFY_FULLY_MUTED_ISSUES"
 
   issues_filter {
-    name = ""
+    name = "tsumugu policy"
     type = "FILTER"
 
     predicate {
       attribute = "labels.policyIds"
       operator  = "EXACTLY_MATCHES"
-      values = [
-        "5087508",
-        newrelic_alert_policy.tsumugu.id,
-      ]
+      values    = [newrelic_alert_policy.tsumugu.id]
     }
   }
 
   destination {
-    channel_id = local.hasunosora_twitter_watch_slack_channel_id
+    channel_id = newrelic_notification_channel.tsumugu_slack.id
     notification_triggers = [
       "ACKNOWLEDGED",
       "ACTIVATED",
